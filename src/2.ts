c8eed2c5f54860ea6a889435638fd67a8a843b03
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-10-23 20:06:33 +0300

    d

--- src/2.ts
+++ src/2.ts
@@ -16,14 +16,19 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  // public name: string;
+  // private department: string;
+  // protected salary: number;
+
+  constructor(
+    public name: string,
+    private department: string,
+    public salary: number
+  ) {
+    // this.name = name;
+    // this.department = department;
+    // this.salary = salary;
   }
 
   getEmployeeDetails() {
@@ -32,8 +37,8 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, (salary += 10000));
+  }
 }
-
-
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): boolean {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +51,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
